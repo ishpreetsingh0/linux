5d84476b60d072ca087810c601767c2c5ecf36fc
Ishpreet Singh <59229503+ishpreetsingh0@users.noreply.github.com>
2023-07-16 21:45:11 +0530

    Update read.sh
    read example from user

--- read.sh
+++ read.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 read -t 20 -p "eneter username in 20 seconds :" username
 echo "hello $username"
+echo "you have successfully taken input from user in 20 seconds, other wise no input from you"
