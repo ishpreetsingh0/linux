254a700e185e26a1f0dbf7705ea4bb8e63ed0620
Ishpreet Singh <59229503+ishpreetsingh0@users.noreply.github.com>
2023-07-31 17:59:15 +0530

    if_else example

---
+++ if_else.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+count=100
+if [ $count -eq 100 ]; then
+	echo "Count is 100"
+else
+	echo "count is not 101"
+fi
+
+if [ $count -eq 101 ]; then
+        echo "Count is 100"
+else
+        echo "count is not 101"
+fi
+
+
