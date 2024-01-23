0becfaf53fb15135bf6ecf53d04273059d4fedcd
Ishpreet Singh <59229503+ishpreetsingh0@users.noreply.github.com>
2024-01-23 10:03:22 +0530

    Update arg.sh

--- arg.sh
+++ arg.sh
@@ -24,3 +24,13 @@ for arg in "$@"
 do
 echo "$arg"
 done
+
+# #!/bin/sh
+# echo "Script Name: $0"
+# echo "First Parameter of the script is $1"
+# echo "The second Parameter is $2"
+# echo "The complete list of arguments is $@"
+# echo "Total Number of Parameters: $#"
+# echo "The process ID is $$"
+# echo "Exit code for the script: $?"
+# */
